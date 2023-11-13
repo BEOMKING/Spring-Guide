2603f9cf618b9d5ef8e56f38d88769882a0edde3
BEOMKING <qjawlsqjacks@naver.com>
2023-11-13 22:13:37 +0900

    ApplicationContextRunner를 활용한 개선된 @Conditional 학습 테스트

--- src/test/java/com/spring/guide/learningtest/config/ConditionalAndExpressionTest.java
+++ src/test/java/com/spring/guide/learningtest/config/ConditionalAndExpressionTest.java
@@ -2,66 +2,51 @@ package com.spring.guide.learningtest.config;
 
 import lombok.Getter;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.context.annotation.UserConfigurations;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringJUnitConfig(ConditionalAndExpressionTest.ConditionalConfig.class)
-@TestPropertySource(properties = {"product.premium.purchase=true"})
 class ConditionalAndExpressionTest {
 
-    @Autowired
-    private ConditionalConfig conditionalConfig;
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(UserConfigurations.of(ConditionalConfig.class));
 
-    @Nested
-    @DisplayName("ConditionalConfig가 활성화되면")
-    class ActivateConditionalConfigTest {
-        @Test
-        @DisplayName("ConditionalConfig 빈이 생성된다.")
-        void conditionalConfigBeanIsCreated() {
-            assertThat(conditionalConfig).isNotNull();
-        }
-        @Test
-        @DisplayName("Property에 키값을 누락했을 시 Expression의 기본 값이 있으면 기본 값을 반환한다.")
-        void expressionActivateConfigWithDefaultValue() {
-            assertThat(conditionalConfig.getHasDefaultValue()).isEqualTo("Basic");
-        }
+    @Test
+    @DisplayName("Expression 값이 존재하면 ConditionalConfig 빈이 생성된다.")
+    void conditionalConfigBeanIsCreated() {
+        contextRunner.withPropertyValues("product.premium.purchase=true")
+                .run(context -> {
+                    assertThat(context).hasSingleBean(ConditionalConfig.class);
+                    assertThat(context.getBean(ConditionalConfig.class)).isNotNull();
+                });
+    }
 
-        @Test
-        @DisplayName("Property에 키값을 누락했을 시 Expression의 기본 값이 없으면 에러를 발생시킨다.")
-        void expressionActivateConfigWithoutDefaultValue() {
-            assertThat(conditionalConfig.getNoDefaultValue()).isEqualTo("${product.premium.grade}");
-        }
+    @Test
+    @DisplayName("Expression 값을 누락해도 기본 값 설정이 있으면 기본 값을 반환한다.")
+    void expressionActivateConfigWithDefaultValue() {
+        contextRunner.withPropertyValues("product.premium.purchase=true")
+                .run(context -> assertThat(context.getBean(ConditionalConfig.class).getHasDefaultValue()).isEqualTo("Basic"));
+    }
+
+    @Test
+    @DisplayName("Expression 값을 누락하고 Expression의 기본 값이 없으면 에러를 발생시킨다.")
+    void expressionActivateConfigWithoutDefaultValue() {
+        contextRunner.withPropertyValues("product.premium.purchase=true")
+                .run(context -> assertThat(context.getBean(ConditionalConfig.class).getNoDefaultValue()).isEqualTo("${product.premium.grade}"));
 
     }
 
-    @Nested
-    @DisplayName("ConditionalConfig가 비활성화되면")
-    @TestPropertySource(properties = {"product.premium.purchase=false"})
-    class DeactivateConditionalConfigTest {
-        @Autowired
-        private ConditionalConfig conditionalConfig;
-
-        @Test
-        @DisplayName("ConditionalConfig 빈이 생성되지 않는다.")
-        void conditionalConfigBeanIsNotCreated() {
-            assertThat(conditionalConfig).isNull();
-        }
-
-        @Test
-        @DisplayName("Property에 키값이 누락되어도 에러를 발생시키지 않는다.")
-        void expressionDeactivateConfig() {
-            assertThatNoException().isThrownBy(() -> conditionalConfig.getNoDefaultValue());
-        }
-
+    @Test
+    @DisplayName("Expression 값이 누락되면 ConditionalConfig 빈이 생성되지 않는다.")
+    void conditionalConfigBeanIsNotCreated() {
+        contextRunner.run(context -> assertThat(context).doesNotHaveBean(ConditionalConfig.class));
     }
 
     @TestConfiguration
